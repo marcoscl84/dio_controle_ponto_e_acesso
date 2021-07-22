b0e60e8c343cdb576d5eaf6f10b7b4468acfef02
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-07-22 20:02:40 -0300

    novos ajustes e classes

--- livedio/src/main/java/com/dio/livedio/model/BancoHoras.java
+++ livedio/src/main/java/com/dio/livedio/model/BancoHoras.java
@@ -2,6 +2,12 @@ package com.dio.livedio.model;
 
 import lombok.*;
 
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -10,4 +16,19 @@ import lombok.*;
 @Builder
 public class BancoHoras {
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class BancoHorasId implements Serializable{
+        private Long idBancoHoras;
+        private Long idMovimento;
+        private Long idUsuario;
+    }
+
+    @EmbeddedId
+    private BancoHorasId id;
+    private LocalDateTime dataTrabalhada;
+    private BigDecimal quantidadeHoras;
+    private BigDecimal saldoHoras;
 }
